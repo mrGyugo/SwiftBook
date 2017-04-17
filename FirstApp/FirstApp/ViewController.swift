f571af213d6a2c3fec0dbb8e08a3b9d9bef789a4
Viktor Mishustin <mrgyugo@gmail.com>
2017-04-17 18:21:18 +0300

    learn stack view

--- FirstApp/FirstApp/ViewController.swift
+++ FirstApp/FirstApp/ViewController.swift
@@ -9,17 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    @IBAction func actionCustomButton(_ sender: UIButton) {
+        
+        let alert = UIAlertController(title: "Hello", message: "Hello world App", preferredStyle: .alert)
+        let  alertAction = UIAlertAction(title: "ОК", style: .default, handler: nil)
+        alert.addAction(alertAction)
+        present(alert, animated: true, completion: nil)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
